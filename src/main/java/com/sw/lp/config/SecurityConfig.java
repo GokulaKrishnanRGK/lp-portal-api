e7f1700b30b16f684a1e1d28b84e733e2bdda70d
Gokul <gokulshyam567@gmail.com>
2024-05-27 04:03:32 -0400

    feat: security config, machine form submit [TEMP]

---
+++ src/main/java/com/sw/lp/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.sw.lp.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.cors.CorsConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  DataSource datasource;
+
+  public SecurityConfig(DataSource datasource) {
+    this.datasource = datasource;
+  }
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests(req -> req.anyRequest().permitAll());
+    http.cors(c -> c.configurationSource(request -> {
+      CorsConfiguration cors = new CorsConfiguration();
+      cors.setAllowedOriginPatterns(Collections.singletonList("*"));
+      cors.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+      cors.setAllowedHeaders(Arrays.asList("Origin", "X-Requested-With", "Content-Type", "Accept", "Access-Control-Allow-Origin",
+          "Access-Control-Allow-Methods", "Access-Control-Allow-Headers", "X-XSRF-Token"));
+      cors.setAllowCredentials(true);
+      return cors;
+    }));
+    return http.build();
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager() {
+    DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+    transactionManager.setDataSource(datasource);
+    return transactionManager;
+  }
+
+}
